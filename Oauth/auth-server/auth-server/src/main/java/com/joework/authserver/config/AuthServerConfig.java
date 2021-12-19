aae042dcf0f8309188801dd4039ad136a89b8da1
Youssif Essam <yousfessaam2323@gmail.com>
2021-12-18 16:24:45 -0800

    implementing OAuth using the grant type 'password'

---
+++ Oauth/auth-server/auth-server/src/main/java/com/joework/authserver/config/AuthServerConfig.java
@@ -0,0 +1,41 @@
+package com.joework.authserver.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    /*
+     this method is used to configure the client credentials like clientId,
+     secret( secret is exactly like passwords) and scope of that client
+     NOTE: Clients are any httpclient like mobile, browser, IOT, etc
+    */
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+
+        clients.inMemory()
+                .withClient("client1")
+                .secret("pass123")
+                .scopes("read")
+                .authorizedGrantTypes("password"); // this will use the grantType => password
+    }
+
+
+    // this method is used to attach the authentication manager to the authServerConfig
+    // as the authentication manager is the link between the authServer and the UserDetailsService
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+    }
+}
---
+++ Oauth/auth-server/auth-server/src/main/java/com/joework/authserver/config/UserManagementConfig.java
@@ -0,0 +1,45 @@
+package com.joework.authserver.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.List;
+
+@Configuration
+public class UserManagementConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        var uds = new InMemoryUserDetailsManager();
+
+        uds.createUser(new User(
+                "joeessam",
+                "pass123",
+                List.of(new SimpleGrantedAuthority("read"))
+        ));
+
+        return uds;
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
